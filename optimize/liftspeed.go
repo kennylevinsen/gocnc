d22147886d324f5a21285b847b60996b857d44a9
Kenny Levinsen <kennylevinsen@gmail.com>
2014-12-02 03:29:16 +0100

    Cleanup

--- optimize/liftspeed.go
+++ optimize/liftspeed.go
@@ -1,17 +1,18 @@
 package optimize
 
 import "github.com/joushou/gocnc/vm"
+import "github.com/joushou/gocnc/vector"
 
 // Uses rapid move for all Z-up only moves.
 // Scans all positions for moves that only change the z-axis in a positive direction,
 // and sets the moveMode to vm.MoveModeRapid.
 func OptLiftSpeed(machine *vm.Machine) {
-	var lastx, lasty, lastz float64
+	var last vector.Vector
 	for idx, m := range machine.Positions {
-		if m.X == lastx && m.Y == lasty && m.Z > lastz {
+		if m.X == last.X && m.Y == last.Y && m.Z > last.Z {
 			// We got a lift! Let's make it faster, shall we?
 			machine.Positions[idx].State.MoveMode = vm.MoveModeRapid
 		}
-		lastx, lasty, lastz = m.X, m.Y, m.Z
+		last = m.Vector()
 	}
 }
